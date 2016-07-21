aa5d180ae4b7fdd18b04dda56f90460c6dd3fab6
feizheng <1290657123@qq.com>
2016-07-21 21:38:14 +0800

    Automatic update alias config

--- src/apache/index.sh
+++ src/apache/index.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-alias e-vhost='sudo vim /private/etc/apache2/extra/httpd-vhosts.conf';
+alias e-httpd='sudo atom /private/etc/apache2/httpd.conf';
+alias e-vhost='sudo atom /private/etc/apache2/extra/httpd-vhosts.conf';
 alias apache-start='sudo apachectl start';
 alias apache-stop='sudo apachectl stop';
 alias apache-reload='sudo apachectl restart';
