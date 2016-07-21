cadcc4139ea2ea47287f30cb64e9547371721ab5
feizheng <1290657123@qq.com>
2016-07-21 16:50:46 +0800

    Automatic update alias config

--- .ssh/init_config.sh
+++ .ssh/init_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-touch ~/.ssh/config
-chmod 600 ~/.ssh/config
+touch ~/.ssh/config;
+chmod 600 ~/.ssh/config;
 
 
 ##create oschina:
@@ -10,6 +10,17 @@ ssh-keygen -t rsa -f ~/.ssh/id_rsa.oschina.net -C "Key for git.oschina.net";
 ##create github:
 ssh-keygen -t rsa -f ~/.ssh/id_rsa.github.com -C "Key for git.github.com";
 
+##create git.zaijiadd.com:
+ssh-keygen -t rsa -f ~/.ssh/id_rsa.zaijiadd.com -C "feizheng@zaijiadd.com";
+
+
+##add to cache:
+ssh-add ~/.ssh/id_rsa.oschina.net;
+ssh-add ~/.ssh/id_rsa.github.com;
+ssh-add ~/.ssh/id_rsa.zaijiadd.com;
+
+
 ##Test git config:
 ssh -T git@git.oschina.net;
 ssh -T git@github.com;
+ssh -T git@git.zaijiadd.com;
