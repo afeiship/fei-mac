7e5131c38ad40e3db4462ee416df96ec510eb1ed
feizheng <1290657123@qq.com>
2016-08-03 23:13:05 +0800

    update svn

--- src/base/cd.sh
+++ src/base/cd.sh
@@ -2,6 +2,8 @@
 
 alias cd-osc='cd ~/git-oschina';
 alias cd-git='cd ~/github';
+alias cd-svn='cd ~/svn';
+
 #yunpan
 alias cd-yunpan='cd ~/yunpan';
 alias cd-tmp='cd /Users/feizheng/git-oschina/temp';
