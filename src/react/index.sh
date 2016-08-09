e46eb0d8b29c3555fcd4231d7b6ddece337677db
feizheng <1290657123@qq.com>
2016-08-09 12:04:08 +0800

    update react

---
+++ src/react/index.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+source ~/github/fei-mac/src/react/react-native.sh;
---
+++ src/react/react-native.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+alias rn='react-native';
+alias rn-r='react-native run-ios';
+alias rn-r-android='react-native run-android';
