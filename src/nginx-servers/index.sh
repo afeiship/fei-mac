99fef192bbe1807b7724f4bde82a04b753fa9c2e
feizheng <1290657123@qq.com>
2016-08-09 20:37:14 +0800

    update nginx-servers

---
+++ src/nginx-servers/index.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+## mv /usr/local/etc/nginx/servers /Users/feizheng/github/fei-mac/src/nginx-servers
+## ln -s /Users/feizheng/github/fei-mac/src/nginx-servers /usr/local/etc/nginx/servers
