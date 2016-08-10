ad9a68134065ae9614500b822934fc88aa5e4884
feizheng <1290657123@qq.com>
2016-08-10 16:27:29 +0800

    update npm-uninstall

--- src/nodejs/index.sh
+++ src/nodejs/index.sh
@@ -8,7 +8,13 @@ alias npm-clean='rm -rf node_modules';
 alias npm-remove='npm cache clean && rm -rf node_modules';
 alias e-npmrc='vim ~/.npmrc';
 alias nbi='bower install && npm install';
+
 alias ni='npm install';
 alias ni-g='npm install -g';
 alias ni-d='npm install --save-dev';
 alias ni-s='npm install --save';
+
+alias nu='npm uninstall';
+alias nu-g='npm uninstall -g';
+alias nu-d='npm uninstall --save-dev';
+alias nu-s='npm uninstall --save';
