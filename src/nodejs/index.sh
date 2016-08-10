98ecc0ca44bfdbeb0dc068f98a7fcd9703050f46
feizheng <1290657123@qq.com>
2016-08-10 16:15:04 +0800

    udpate nodejs

--- src/nodejs/index.sh
+++ src/nodejs/index.sh
@@ -8,3 +8,7 @@ alias npm-clean='rm -rf node_modules';
 alias npm-remove='npm cache clean && rm -rf node_modules';
 alias e-npmrc='vim ~/.npmrc';
 alias nbi='bower install && npm install';
+alias ni='npm install';
+alias ni-g='npm install -g';
+alias ni-d='npm install --save-dev';
+alias ni-s='npm install --save';
