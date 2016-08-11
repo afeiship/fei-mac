8134160dc916860b3a0cbd5bcf813fa97219b033
feizheng <1290657123@qq.com>
2016-08-11 12:36:10 +0800

    add alfred configs

---
+++ src/alfred/init_config.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+mv "/Users/feizheng/Library/Application Support/Alfred 2" /Users/feizheng/github/fei-mac/src/alfred
+ln -s "/Users/feizheng/github/fei-mac/src/alfred/Alfred 2" "/Users/feizheng/Library/Application Support/Alfred 2"
