ad9af891318a64f5a41c57b48ccf8fdfd66b5aa7
feizheng <1290657123@qq.com>
2016-08-14 15:53:14 +0800

    Automatic update alias config

--- src/apache/index.sh
+++ src/apache/index.sh
@@ -4,3 +4,4 @@ alias e-vhost='sudo atom /private/etc/apache2/extra/httpd-vhosts.conf';
 alias apache-start='sudo apachectl start';
 alias apache-stop='sudo apachectl stop';
 alias apache-reload='sudo apachectl restart';
+alias apache-check='ps aux | grep httpd';
