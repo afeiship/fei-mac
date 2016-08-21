6ed5c6d90f3c3e79a612c8dd694072e8b181e032
feizheng <1290657123@qq.com>
2016-08-21 22:54:51 +0800

    Automatic update alias config

--- src/base/index.sh
+++ src/base/index.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#init:
+archey;
 
 #base
 alias ll='ls -al';
---
+++ src/motd/init_config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo mv /etc/motd /Users/feizheng/github/fei-mac/src/motd;
+sudo ln -s /Users/feizheng/github/fei-mac/src/motd/motd /etc/motd;
+
+brew install archey;
