f17afa8db13d4452e0911b9017f773ede54c6211
feizheng <1290657123@qq.com>
2016-09-14 12:16:06 +0800

    Automatic update alias config

--- src/mackup/index.sh
+++ src/mackup/index.sh
@@ -2,3 +2,4 @@
 alias mac-backup='mackup -f backup';
 alias mac-restore='mackup restore';
 alias mac-bak-ls='ls -alh /Users/feizheng/yunpan/mackup/mac/Mackup';
+alias e-mackup='atom /Users/feizheng/yunpan/mackup/mac/Mackup';
