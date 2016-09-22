92c5bef893c6adcb0cf6b5ac561a10cab97fba5a
feizheng <1290657123@qq.com>
2016-09-22 13:31:54 +0800

    remoev ushell modules

--- ushell_modules/ushell-module-base/cd.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-alias ..='cd ..';
-alias cd-='cd -';
-alias cd-osc='cd ~/git-oschina';
-alias cd-git='cd ~/github';
--- ushell_modules/ushell-module-base/index.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-ROOT_PATH=$(pwd);
-
-alias usm-base-reload="source ${ROOT_PATH}/index.sh";
-source $ROOT_PATH/cd.sh;
-source $ROOT_PATH/chmod.sh;
