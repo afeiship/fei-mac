198bc46792133e7d6d1f30c384bb8a8cd5458344
afeiship <1290657123@qq.com>
2018-04-28 10:45:08 +0800

    issue: bower -> npm

--- src/index.sh
+++ src/index.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-## initail ushell_modules:
+## initail node_modules:
 BASH_BASE_PATH=~/github/fei-mac;
 
-for dir in $(ls $BASH_BASE_PATH/ushell_modules)
+for dir in $(ls $BASH_BASE_PATH/node_modules)
 do
-  if [ -d $BASH_BASE_PATH/ushell_modules/$dir ]; then
+  if [ -d $BASH_BASE_PATH/node_modules/$dir ]; then
     # echo $dir;
-    source $BASH_BASE_PATH/ushell_modules/$dir/index.sh;
+    source $BASH_BASE_PATH/node_modules/$dir/index.sh;
   fi
 done
 
