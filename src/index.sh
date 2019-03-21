d3790b8e08adba8fa94b4d257c9abcfca95e35a0
afeiship <1290657123@qq.com>
2019-03-21 12:01:40 +0800

    add: use code edit fei-mac

--- src/index.sh
+++ src/index.sh
@@ -21,7 +21,7 @@ done
 
 
 #Edit file
-alias e-alias='atom $BASH_BASE_PATH';
+alias e-alias='code $BASH_BASE_PATH';
 alias alias-reload='source $BASH_BASE_PATH/src/index.sh';
 alias alias-backup='cd $BASH_BASE_PATH/ && gg "Automatic update alias config" && cd -';
 alias macfiles-backup='cd ~/git-oschina/backup-mac-files/ && gg "Automatic backup mac files" && cd -';
