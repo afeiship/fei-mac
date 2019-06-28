23a5bc96eb3f4b390a4b38a875dde1301c89a15a
afeiship <1290657123@qq.com>
2019-06-28 14:23:20 +0800

    fix: remove mac-backupfiles

--- src/initialize.sh
+++ src/initialize.sh
@@ -4,9 +4,5 @@
 cd ~/github
 git clone git@github.com:afeiship/fei-mac.git
 
-## install macfiles-backup
-cd ~/git-oschina
-git clone git@git.oschina.net:afeiship/backup-mac-files.git
-
 ## install dotfiles:
 git clone git@github.com:afeiship/dotfiles.git ~/.dotfiles
