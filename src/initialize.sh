9fac24a34fe4812e777fb0819cc71c4ffd8090e9
afeiship <1290657123@qq.com>
2020-06-01 06:48:04 +0800

    fix: remove intial

--- src/initialize.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-## install fei-mac:
-cd ~/github
-git clone git@github.com:afeiship/fei-mac.git
-
-## install dotfiles:
-git clone git@github.com:afeiship/dotfiles.git ~/.dotfiles
