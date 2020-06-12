0f4c8e5e583be86c23dba0612dd8d043d02854db
afeiship <1290657123@qq.com>
2020-06-12 13:41:52 +0800

    fix: mac-settings from shell

--- src/index.sh
+++ src/index.sh
@@ -3,19 +3,10 @@
 ## initail node_modules:
 BASH_BASE_PATH=~/github/mac-settings;
 
-# for scoped ushell-module @feizheng
-for dir in $(ls $BASH_BASE_PATH/node_modules/@feizheng)
+# for scoped ushell-module @afeiship/@feizheng
+for dir in $(ls $BASH_BASE_PATH/node_modules/@*fei*)
 do
-    if [ -d $BASH_BASE_PATH/node_modules/@feizheng/$dir ]; then
-        source $BASH_BASE_PATH/node_modules/@feizheng/$dir/index.sh;
-    fi
-done
-
-
-# for scoped ushell-module @afeiship
-for dir in $(ls $BASH_BASE_PATH/node_modules/@afeiship)
-do
-    if [ -d $BASH_BASE_PATH/node_modules/@afeiship/$dir ]; then
-        source $BASH_BASE_PATH/node_modules/@afeiship/$dir/index.sh;
+    if [ -d $BASH_BASE_PATH/node_modules/@*fei*/$dir ]; then
+        source $BASH_BASE_PATH/node_modules/@*fei*/$dir/index.sh;
     fi
 done
