69b17132896fb79ad666fa62fd6520b9b7b74ff0
afeiship <1290657123@qq.com>
2020-06-12 13:30:58 +0800

    feat: add private pkg

--- src/index.sh
+++ src/index.sh
@@ -6,10 +6,19 @@ BASH_BASE_PATH=~/github/mac-settings;
 # ushell-modules 
 source $BASH_BASE_PATH/node_modules/ushell-module-secret/index.sh;
 
-# for scoped ushell-module
+# for scoped ushell-module @feizheng
 for dir in $(ls $BASH_BASE_PATH/node_modules/@feizheng)
 do
     if [ -d $BASH_BASE_PATH/node_modules/@feizheng/$dir ]; then
         source $BASH_BASE_PATH/node_modules/@feizheng/$dir/index.sh;
+    fi
+done
+
+
+# for scoped ushell-module @afeiship
+for dir in $(ls $BASH_BASE_PATH/node_modules/@afeiship)
+do
+    if [ -d $BASH_BASE_PATH/node_modules/@afeiship/$dir ]; then
+        source $BASH_BASE_PATH/node_modules/@afeiship/$dir/index.sh;
     fi
 done
